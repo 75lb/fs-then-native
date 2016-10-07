2e9a1ba4b606c94dc1090c629f48f5ee567e491f
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-10-07 10:23:54 +0100

    fix tests

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var detect = require('feature-detect-es6')
 
-if (detect.all('class', 'arrowFunction', 'let', 'const')) {
+if (detect.all('class', 'arrowFunction', 'let', 'const', 'spread')) {
   module.exports = require('./src/lib/fs-then-native')
 } else {
   module.exports = require('./es5/lib/fs-then-native')
